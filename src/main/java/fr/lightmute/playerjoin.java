bc19cf34a8ea9f3ea9ee93c8c4fef1400a2ef7ea
LightMute <admin@admin>
2020-06-24 14:57:31 +0200

    Save

---
+++ src/main/java/fr/lightmute/playerjoin.java
@@ -0,0 +1,19 @@
+package fr.lightmute;
+
+import org.bukkit.Color;
+
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.LoginEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+
+public class playerjoin implements Listener {
+	
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void playerjoinevents(LoginEvent e) {
+		ProxiedPlayer player = (ProxiedPlayer) e.getConnection();
+		player.sendMessage(player.getName()+ Color.fromRGB(34, 24, 17) + " coucou");
+	}
+
+}
